156202873b2e9d2dbf0253da614629b72240b02a
Derrick Low <derrick@sonic.net>
2023-03-25 12:55:19 -0700

    day 3 updates

--- src/index.js
+++ src/index.js
@@ -17,6 +17,64 @@ async function init() {
     style.layers.push(...custom.layers);
     map.setStyle(style);
     map.getSource("neighborhoods").setData(neighborhoods);
+    
+    initLegend();
+    initPopup();
+}
+
+function initLegend() {
+    const legend = document.querySelector("#legend");
+    const template = document.querySelector("#legend-entry")
+    const fillColorStyle = map.getPaintProperty("neighborhoods-fill-extrusion", "fill-extrusion-color");
+    
+    fillColorStyle.splice(0,2); // remove first two elements: ["step", ["get","count"]]
+    let lowerBound = 1;
+    for (let index = 0; index < fillColorStyle.length; index+=2) {
+        const legendEntry = document.importNode(template.content, true);
+        const entrySpans = legendEntry.querySelectorAll("span");
+        const hexColorCode = fillColorStyle[index];
+        const upperBound = fillColorStyle[index+1];
+
+        entrySpans[0].style.backgroundColor = hexColorCode;
+        if (upperBound) {
+            entrySpans[1].textContent = lowerBound + " - " + (upperBound - 1) + " sites";
+        } else {
+            entrySpans[1].textContent = ">=" + lowerBound + " sites";
+        }
+
+        lowerBound = upperBound;
+        legend.appendChild(legendEntry);
+    }
+
+}
+
+let hovered;
+const popup = document.querySelector("#popup");
+const neighborhoodName = popup.querySelector(".neighborhood-name");
+const nieghborhoodSiteCount = popup.querySelector(".neighborhood-site-count");
+function initPopup() {
+    map.on('mousemove', 'neighborhoods-fill', function(event) {
+        clearHover();
+        if (event.features.length > 0) {
+            hovered = event.features[0];
+            neighborhoodName.textContent = hovered.properties.name;
+            nieghborhoodSiteCount.textContent = hovered.properties.count;
+            popup.style.display = "block";
+            map.setFeatureState(hovered, {
+                hover: true
+             })
+        }
+    });
+    map.on('mouseleave', 'neighborhoods-fill', clearHover);
+}
+
+function clearHover() {
+    popup.style.display = "none";
+    if (hovered) {
+        map.setFeatureState(hovered, {
+            hover: false
+        })
+    }
 }
 
 mapboxgl.accessToken = settings.accessToken;
